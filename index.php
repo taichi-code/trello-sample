994cce67753a75acb3f62d4a92fed0a9dc67f4aa
taichi-tsunekawa <wp.tsunekawa@gmail.com>
2022-06-18 02:12:18 +0900

    ひな形追加

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initiascale=1.0,user-scalable=no">
+    <meta name="description" content="サイト説明" />
+    <title>サイトタイトル</title>
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="js/swiper/swiper.min.css">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/top.css">
+</head>
+
+<body>
+    <!-- HEADER-------------------------- -->
+    <header class="header">
+        <div class="header__logo">
+            <h1 class="logo">
+                <a href="#" class="logo__link"><img src="images/logo.svg" class="logo__img" alt="PDN DESIGN"></a>
+            </h1>
+        </div>
+        <nav class="global-nav">
+            <ul class="global-nav__list">
+                <li class="global-nav__item">
+                    <a href="#" class="global-nav__link">news</a>
+                </li>
+                <li class="global-nav__item">
+                    <a href="#" class="global-nav__link">service</a>
+                </li>
+                <li class="global-nav__item">
+                    <a href="#" class="global-nav__link">works</a>
+                </li>
+                <li class="global-nav__item">
+                    <a href="#" class="global-nav__link">company</a>
+                </li>
+                <li class="global-nav__item">
+                    <a href="#" class="global-nav__link">recruit</a>
+                </li>
+                <li class="global-nav__item">
+                    <a href="#" class="global-nav__link">contact</a>
+                </li>
+            </ul>
+        </nav>
+        <button type="button" class="btn-menu">
+            <span class="btn-menu__line"></span>
+        </button>
+    </header>
+
+
+    <!-- MAIN---------------------------- -->
+    <main class="main">
+
+    </main>
+    <!-- FOOTER-------------------------- -->
+    <footer class="footer">
+
+    </footer>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/swiper/swiper.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(function () {
+
+    //ハンバーガーメニュー
+    var btnMenu = $('.btn-menu');
+    var globalNav = $('.global-nav');
+
+    btnMenu.on('click', function () {
+        btnMenu.toggleClass('active');
+        globalNav.toggleClass('show');
+    });
+
+    //ヘッダースクロール
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.header').addClass('active');
+        } else {
+            $('.header').removeClass('active');
+        }
+    });
+
+    // ページトップ
+    var $pageTop = $('.page-top');
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $pageTop.fadeIn();
+        } else {
+            $pageTop.fadeOut();
+        }
+    });
+    $pageTop.on('click', function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 300);
+        return false;
+    });
+
+});
